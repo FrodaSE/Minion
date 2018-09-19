21849d680dd77be895453d951a60d21994d2a01a
Tim Iskhakov <timiskhakov@gmail.com>
2018-09-19 11:54:42 +0300

    Move resolver to ExecuteAsync()

--- src/Minion.Core/DependencyInjectionJobExecutor.cs
+++ src/Minion.Core/DependencyInjectionJobExecutor.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -10,44 +9,35 @@ namespace Minion.Core
 {
 	internal class DependencyInjectionJobExecutor : IJobExecutor
 	{
-		private readonly IDependencyResolver _resolver;
-
-		public DependencyInjectionJobExecutor(IDependencyResolver resolver)
-		{
-			_resolver = resolver;
-		}
-
-		public Task<JobResult> ExecuteAsync(JobDescription jobDescription)
+		public Task<JobResult> ExecuteAsync(JobDescription jobDescription, IDependencyResolver resolver = null)
 		{
 			var type = Type.GetType(jobDescription.Type);
-
 			var typeInfo = type.GetTypeInfo();
+			ConstructorInfo ctor = typeInfo.GetConstructors().Single();
+			ParameterInfo[] parameters = ctor.GetParameters();
 
-			var ctor = typeInfo.GetConstructors().Single();
-
-			var arguments = new List<object>();
-
-			foreach (var param in ctor.GetParameters())
+			if (parameters.Length > 0 && resolver == null)
 			{
-			    if (_resolver == null)
-			        throw new InvalidOperationException("Cannot resolve dependencies without a dependency resolver.");
-
-				if (!_resolver.Resolve(param.ParameterType, out var resolvedType))
-					throw new InvalidOperationException("Could not resolve parameter of type: " + param.ParameterType.AssemblyQualifiedName);
-
-				arguments.Add(resolvedType);
+				throw new InvalidOperationException("Cannot resolve dependencies without a dependency resolver.");
 			}
+				
+			var arguments = parameters
+				.Select(p => resolver.Resolve(p.ParameterType, out var resolvedType)
+					? resolvedType
+					: throw new InvalidOperationException(
+						$"Could not resolve parameter of type: {p.ParameterType.AssemblyQualifiedName}"))
+				.ToArray();
 
-			var jobInstance = ctor.Invoke(arguments.ToArray());
+			object jobInstance = ctor.Invoke(arguments);
 
 			switch (jobInstance)
 			{
-			    case Job job:
-			        return job.ExecuteAsync();
-			    case JobInputBase inputJob:
-			        return inputJob.DoExecuteAsync(jobDescription.Input.InputData);
-			    default:
-			        throw new InvalidOperationException("Unknown job type.");
+				case Job job:
+					return job.ExecuteAsync();
+				case JobInputBase inputJob:
+					return inputJob.DoExecuteAsync(jobDescription.Input.InputData);
+				default:
+					throw new ArgumentOutOfRangeException(nameof(jobInstance), "Unknown job type.");
 			}
 		}
 	}
